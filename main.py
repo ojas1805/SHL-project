42ab3ddfc3e86618be10c4b5881f44096e59b8e9
Your Name <your@email.com>
2026-06-30 23:56:23 +0530

    test simple app

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 from fastapi import FastAPI
-from retrieval import Retriever
 
 app = FastAPI()
 
-retriever = Retriever()
-
 @app.get("/")
 def home():
-    return {"message": "SHL Recommender API is running"}
+    return {"message": "working"}
 
 @app.get("/health")
 def health():
     return "OK"
-
-@app.get("/recommend")
-def recommend(query: str):
-    results = retriever.search(query)
-    return {"recommendations": results}
